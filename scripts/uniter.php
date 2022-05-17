b8d967b72450ae72e129050bffd86c681b3c4e22
Dmitriy Ivashchenko <gen.style22@gmail.com>
2022-05-17 13:15:58 +0300

    first version

---
+++ scripts/uniter.php
@@ -0,0 +1,9 @@
+<?php
+
+file_put_contents('united.html',
+	file_get_contents('messages.html')#.
+	#file_get_contents('messages1.html').
+	#file_get_contents('messages2.html').
+	#file_get_contents('messages3.html')
+	);
+?>
